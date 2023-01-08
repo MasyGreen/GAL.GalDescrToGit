ca69968d556b0401fde715050194932116c1e026
masygreen <masygreen@gmail.com>
2023-01-08 22:05:52 +0300

    to html email

--- GalDescrToGit.py
+++ GalDescrToGit.py
@@ -7,7 +7,9 @@ import threading
 from ftplib import FTP
 from queue import Queue
 from colorama import Fore
-import hashlib
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 # Addition class - print message
@@ -38,8 +40,11 @@ class AppSettings:
         self.MailSMTPServer: str = ''
         self.MailPassword: str = ''
         self.MailSMTPPort: int = 0
-        self.MailAdr: str = ''
+        self.MailFrom: str = ''
+        self.MailTo: str = ''
+        self.MailAdditionText: str = ''
         self.IsSendMail: bool = False
+        self.IsIncludeNewInMail: bool = False
         self.FTPHost: str = ''
         self.FTPDir: str = ''
 
@@ -163,13 +168,13 @@ class FTPReader:
     # Get maximum file date from FTP
     def GetMaxDateFromFTP(self) -> datetime:
         printmsg.PrintHeader('Start GetMaxDateFromFTP')
-        result: datetime = datetime.datetime.now()
+        result: datetime = datetime.datetime(1, 1, 1, 0, 0)
 
         try:
             ftp = FTP(appsettings.FTPHost)
             print(f"Login to FTP: {appsettings.FTPHost}, try goto {appsettings.FTPDir}. {ftp.login()}")
 
-            maxDateTime = 0  # максимальная дата файла int в формате YYYYMMDD
+            maxDateTime: datetime = datetime.datetime(1, 1, 1, 0, 0)  # максимальная дата файла int в формате YYYYMMDD
 
             files = ftp.mlsd(appsettings.FTPDir)  # Получаем файлы с датами с FTP
             for file in files:
@@ -177,12 +182,13 @@ class FTPReader:
                 fileType = file[1]['type']
                 if fileType == 'file':  # смотрим только файлы
                     timeStamp = file[1]['modify']  # дата модификации файла
-                    curFileDate = int(timeStamp[:8])  # int в формате YYYYMMDD
+                    curFileDate = datetime.datetime.strptime(timeStamp[:14], '%Y%m%d%H%M%S')
+
                     if maxDateTime < curFileDate:
                         maxDateTime = curFileDate
-                        printmsg.PrintDebug(f'FileName = {fileName}, FileType =  {fileType}, {curFileDate}')
+                        printmsg.PrintDebug(f'FileName = {fileName}, FileType =  {fileType}, {curFileDate}/{timeStamp}')
 
-            result = datetime.datetime.strptime(str(maxDateTime), '%Y%m%d')
+            result = maxDateTime
 
             printmsg.PrintSuccess(f'result = {result}')
         except Exception as inst:
@@ -264,7 +270,7 @@ class FTPReader:
                 t.start()
 
             # Даем очереди нужные нам ссылки для скачивания (FTPList[:1])
-            for el in FTPList[:2]:
+            for el in FTPList:
                 queueFTP.put(el)
 
             # Ждем завершения работы очереди
@@ -274,7 +280,7 @@ class FTPReader:
         except:
             printmsg.PrintErrror(f'Download FTP')
 
-
+# Declde file to UTF8
 def DecodeFile():
     # Удалить все файлы *.txt в каталоге назначения где в имени файла нет _WIN1251
     printmsg.PrintHeader(f'Delete old convert file')
@@ -364,9 +370,17 @@ def ReadConfig(filepath):
         appsettings.MailSMTPPort = config.has_option("Settings", varsettingsname) and config.getint("Settings",
                                                                                                     varsettingsname) or 0
 
-        varsettingsname = GetClassValueNameLow(f"{appsettings.MailAdr=}")
-        appsettings.MailAdr = config.has_option("Settings", varsettingsname) and config.get("Settings",
-                                                                                            varsettingsname) or None
+        varsettingsname = GetClassValueNameLow(f"{appsettings.MailFrom=}")
+        appsettings.MailFrom = config.has_option("Settings", varsettingsname) and config.get("Settings",
+                                                                                             varsettingsname) or None
+
+        varsettingsname = GetClassValueNameLow(f"{appsettings.MailTo=}")
+        appsettings.MailTo = config.has_option("Settings", varsettingsname) and config.get("Settings",
+                                                                                           varsettingsname) or None
+
+        varsettingsname = GetClassValueNameLow(f"{appsettings.MailAdditionText=}")
+        appsettings.MailAdditionText = config.has_option("Settings", varsettingsname) and config.get("Settings",
+                                                                                                     varsettingsname) or None
 
         varsettingsname = GetClassValueNameLow(f"{appsettings.FTPHost=}")
         appsettings.FTPHost = config.has_option("Settings", varsettingsname) and config.get("Settings",
@@ -379,6 +393,11 @@ def ReadConfig(filepath):
         varsettingsname = GetClassValueNameLow(f"{appsettings.IsSendMail=}")
         appsettings.IsSendMail = config.has_option("Settings", varsettingsname) and config.getboolean("Settings",
                                                                                                       varsettingsname) or False
+
+        varsettingsname = GetClassValueNameLow(f"{appsettings.IsIncludeNewInMail=}")
+        appsettings.IsIncludeNewInMail = config.has_option("Settings", varsettingsname) and config.getboolean(
+            "Settings",
+            varsettingsname) or False
 
         varsettingsname = GetClassValueNameLow(f"{printmsg.IsPrintDebug=}")
         printmsg.IsPrintDebug = config.has_option("Settings", varsettingsname) and config.getboolean("Settings",
@@ -400,8 +419,14 @@ def ReadConfig(filepath):
         varsettingsname = GetClassValueNameLow(f"{appsettings.MailSMTPPort=}")
         config.set("Settings", varsettingsname, '587')
 
-        varsettingsname = GetClassValueNameLow(f"{appsettings.MailAdr=}")
-        config.set("Settings", varsettingsname, 'test@gmail.com')
+        varsettingsname = GetClassValueNameLow(f"{appsettings.MailFrom=}")
+        config.set("Settings", varsettingsname, 'put@gmail.com')
+
+        varsettingsname = GetClassValueNameLow(f"{appsettings.MailTo=}")
+        config.set("Settings", varsettingsname, 'get@gmail.com')
+
+        varsettingsname = GetClassValueNameLow(f"{appsettings.MailAdditionText=}")
+        config.set("Settings", varsettingsname, 'You can read text from GIT')
 
         varsettingsname = GetClassValueNameLow(f"{appsettings.FTPHost=}")
         config.set("Settings", varsettingsname, 'ftp.galaktika.ru')
@@ -410,6 +435,9 @@ def ReadConfig(filepath):
         config.set("Settings", varsettingsname, 'pub/support/galaktika/bug_fix/GAL910/DESCRIPTIONS')
 
         varsettingsname = GetClassValueNameLow(f"{appsettings.IsSendMail=}")
+        config.set("Settings", varsettingsname, 'false')
+
+        varsettingsname = GetClassValueNameLow(f"{appsettings.IsIncludeNewInMail=}")
         config.set("Settings", varsettingsname, 'false')
 
         varsettingsname = GetClassValueNameLow(f"{printmsg.IsPrintDebug=}")
@@ -422,7 +450,7 @@ def ReadConfig(filepath):
 
         return False
 
-
+# Get list last update file
 def GetLastFileList(workDate: datetime):
     result = []
     printmsg.PrintDebug(f'{workDate=}; {workDate.year}')
@@ -436,7 +464,7 @@ def GetLastFileList(workDate: datetime):
                 timeStamp = os.path.getmtime(os.path.join(path, file))
                 curFileDate = datetime.datetime.fromtimestamp(timeStamp)  # int в формате YYYYMMDD
 
-                if workDate == curFileDate:
+                if noTimeDate == curFileDate:
                     row = {"filename": file,
                            "filepath": f'{os.path.join(path, file)}',
                            "filedatetme": curFileDate}
@@ -445,14 +473,113 @@ def GetLastFileList(workDate: datetime):
 
     return result
 
+# Read new Issue from file
+def GetNewText(lastUpdateFileList: []) -> str:
+    printmsg.PrintHeader('Start GetNewText')
+    result = '\n<h2>Content file</h2>\n'
+    try:
+        for el in lastUpdateFileList:
+            filename = el.get("filename")
+            filepath = el.get("filepath")
+            result += f'<h3>File name: {filename}</h3>\n'
+
+            startI: bool = False  # Начало задачи
+            issueHeader: bool = False  # Начало текста задачи
+            issueText = ''  # Текст задачи
+            isNewIssue: bool = False  # Признак новой задачи
+            skeepFile: bool = False  # Признак пропуска файла, новые задачи вначале - дальше файл можно пропустить
+            index = 0
+            with open(filepath, 'r', encoding='UTF-8') as fr:
+                for line in fr.readlines():
+                    index = index + 1
+                    if skeepFile:
+                        printmsg.PrintDebug(f'Exit line: {index}')
+                        break
+
+                    # Начало задачи
+                    if line.find('* ЗАДАЧА В JIRA:') != -1 and not startI:
+                        issueHeader = True
+                        startI = True
+                        isNewIssue = False
+                        issueText = ''
+
+                    # Конец задачи
+                    if line.find('* * *') != -1 and startI:
+                        startI = False
+
+                        if isNewIssue:
+                            result += f'{issueText}\n'
+
+                    # признак новой задачи
+                    if line.find('* ПЕРВОЕ РЕШЕНИЕ:') != -1 and startI:
+                        if line.find(': NEW') != -1 and startI:
+                            isNewIssue = True
+                        else:
+                            skeepFile = True
+                    if issueHeader:
+                        issueHeader = False
+                        issueText += f'<p><b>{line[:-1]}</b></p>\n'
+                    else:
+                        issueText += f'<p>{line[:-1]}</p>\n'
+
+        printmsg.PrintDebug(f'{result}')
+        printmsg.PrintSuccess(f'Get new text in path')
+    except Exception as inst:
+        printmsg.PrintErrror(f'{type(inst)}')  # the exception instance
+        printmsg.PrintErrror(f'{inst.args}')  # arguments stored in .args
+        printmsg.PrintErrror(f'{inst}')  # __str__ allows args to be printed directly,
+
+    return result
+
+# Send email
+def SendingEmail(workDate: datetime, lastUpdateFileList: []):
+    printmsg.PrintHeader('Start SendingEmail')
+    message = '<html><head></head><body>'
+    message += f"<p>Last check time: {datetime.datetime.now().strftime('%d %b %Y, %H:%M')}</p>\n"
+    message += f"<p>The last modification file date from FTP UTC:<b>{workDate.strftime('%d %b %Y, %H:%M')}</b></p>\n"
+    message += f"<p>{appsettings.MailAdditionText}</p>\n\n"
+
+    message += f"<h2>List update file:</h2>\n"
+    for el in lastUpdateFileList:
+        message += f'<p>   {el.get("filename")}</p>\n'
+
+    try:
+        # Формирование текста сообщения e-mail
+        e_mail_msg = MIMEMultipart()
+        e_mail_msg["From"] = appsettings.MailFrom
+        e_mail_msg["To"] = appsettings.MailTo
+        e_mail_msg["Subject"] = "Update ftp.galaktika.ru"
+
+        if appsettings.IsIncludeNewInMail:
+            message += GetNewText(lastUpdateFileList)
+
+        message += '</body></html>'
+        e_mail_msg.attach(MIMEText(message, 'html'))
+
+        printmsg.PrintDebug(f'{e_mail_msg.as_string()}')
+
+        # Отправка сообщения
+        server = smtplib.SMTP(appsettings.MailSMTPServer, appsettings.MailSMTPPort)
+        server.starttls()
+        server.login(appsettings.MailFrom, appsettings.MailPassword)
+
+        text = e_mail_msg.as_string()
+        server.sendmail(appsettings.MailFrom, appsettings.MailTo, text)
+        server.quit()
+        printmsg.PrintSuccess(f'Sending email')
+    except Exception as inst:
+        printmsg.PrintErrror(f'{type(inst)}')  # the exception instance
+        printmsg.PrintErrror(f'{inst.args}')  # arguments stored in .args
+        printmsg.PrintErrror(f'{inst}')  # __str__ allows args to be printed directly,
+
 
 def main():
     printmsg.PrintHeader('Start work')
 
     IsGetMaxDate = True  # получить дату редактирования с FTP
-    IsDowloadFTP = False  # скачивать файлы с FTP
-    IsDecodeFile = False  # перекодировать файлы
-    IsDeleteDownloadFile = False  # удалять не конвертированные файлы
+    IsDowloadFTP = True  # скачивать файлы с FTP
+    IsDecodeFile = True  # перекодировать файлы
+    IsDeleteDownloadFile = True  # удалять не конвертированные файлы
     IsGetLastFileList = True  # получить список последних обновленных файлов
 
     # максимальная дата файла на FTP
@@ -492,6 +619,8 @@ def main():
         for el in lastUpdateFileList:
             printmsg.PrintDebug(f'{el.get("filepath")}')
 
+    if appsettings.IsSendMail:
+        SendingEmail(ftpMaxDate, lastUpdateFileList)
     printmsg.PrintSuccess('End work')
 
 
