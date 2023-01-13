5fa289690854dc947b7a10a85cf7fef7fdb57915
masygreen <masygreen@gmail.com>
2023-01-13 11:25:42 +0300

    Bat file

---
+++ Remote.bat
@@ -0,0 +1,9 @@
+@ECHO =====================Attention!!!============================
+@ECHO Remote add
+@ECHO =====================Attention!!!============================
+pause
+git remote -v
+git remote rm origin
+git remote add origin git@github.com******.git
+git remote -v
+pause
